e3cf2f8e1940ed8b44c747321067af7c5a7e1ca5
stellerNet <130874769+stellerNet@users.noreply.github.com>
2023-05-11 10:54:30 -0500

    Update movies.js

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,18 +1,21 @@
+let answer = confirm("Would you like to view the movie in full screen?");
+const baseurl = "https://steller-production.up.railway.app";
+const fullurl = baseurl + game;
 const queryString = window.location.search;
 document.body.style.overflow = "hidden";
 const urlParams = new URLSearchParams(queryString);
 const game = urlParams.get('iframe')
-const baseurl = "https://steller-production.up.railway.app";
-const fullurl = baseurl + game;
 function showLoading() {
     document.getElementById('loading').style.display = 'block';
 }
-
 function hideLoading() {
     document.getElementById('loading').style.display = 'none';
 }
 
-if(game != null) {
+if (answer == true) {
+  window.open(fullurl, "_blank");
+} else {
+  if(game != null) {
     document.getElementById("frame").innerHTML = `
         <div id="loading"></div>
         <iframe id="ifr" onload="hideLoading();"></iframe>
@@ -26,4 +29,5 @@ if(game != null) {
     document.getElementById("frame").style.overflow = "hidden";
     alert("Some Shows/Movies Take Longer To Load Than Others");
     document.getElementById("ifr").src = fullurl;
+}
 }
